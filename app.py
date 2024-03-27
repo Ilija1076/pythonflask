aff75f75df4b7595935092af91c31aefe14c1287
Ilija1076 <messi.kralj00@gmail.com>
2024-03-27 18:21:55 +0100

    basics done

--- app.py
+++ app.py
@@ -6,6 +6,28 @@ app = Flask(__name__)
 app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = ""
-app.config["MYSQL_DB"] = "todo"
+app.config["MYSQL_DB"] = "db"
 
 mysql = MySQL(app)
+
+
+def init_db():
+    cur = mysql.connection.cursor()
+    cur.execute(
+        """
+        CREATE TABLE IF NOT EXISTS wishlist(id INT AUTO_INCREMENT PRIMARY KEY, content TEXT)
+        """
+    )
+    mysql.connection.commit()
+    cur.close()
+
+
+@app.route("/")
+def wishes():
+    init_db()
+    cur = mysql.connection.cursor()
+    cur.close()
+    return render_template("wishes.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
